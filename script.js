2ed59e69146c8bdd07b90130de938e7c7a08ea01
zahrevans <526Zevans@frhsd.com>
2024-11-30 23:12:42 +0000

    middle complete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//variables for beginning
 let username = prompt('What is your Name!?');
-let gender = prompt('What is your characters gender');
+let pron = prompt('He or She')
 let adj1b = prompt('Give an adjective');
 let n1b = prompt('Give a Noun');
 let v1b = prompt('Give a Verb');
@@ -11,12 +12,26 @@ let v3b = prompt('Give a Verb');
 
 
 
+//Beginning paragraph
+let beg = `<p> Once upon a time, in a ${adj1b} village, there lived a ${n1b} named ${username}. Every day, ${username} would ${v1b} around the village, dreaming of ${n2b}. One morning, ${username} received an unexpected letter that said, “You have been chosen to find the legendary ${n2b}!” Excited, ${username} packed their ${adj2b} backpack and set off on a journey to find the ${n3b}, determined to ${n3b} it.</p>`;
 
-let beg = `<p> Once upon a time, in a ${adj1b} village, there lived a ${n1b} named ${username}. Every day, ${username} would ${v1b} around the village, dreaming of ${n2b}. One morning, ${username} received an unexpected letter that said, “You have been chosen to ${v2b} the legendary ${n2b}!” Excited, ${username} packed ${gender} ${adj2b} backpack and set off on a journey to find the ${n3b}, determined to ${n3b} it.`;
+//middle variables
+let adj1m = prompt('Give an Adjective');
+let pn1m = prompt('Give a Plural Noun (an animal for a better story flow)');
+let adj2m = prompt('Give an Adjective');
+let adj3m = prompt('Give an Adjective');
+let animal = prompt('Give an Animal');
+let v1m = prompt('Give a Verb');
+let n1m = prompt('Give a Noun');
+let adj4m = prompt('Give an Adjective');
 
+//middle pargraph
+let mid = `<p> During the journey, ${username} faced many challenges. First, ${pron} had to cross a ${adj1m} river full of ${pn1m}. Then, ${username} found a ${adj2m} forest where a ${adj3m} ${animal} warned, “You must ${v1m} the ${n1m} or risk being trapped forever!” With quick thinking and a ${adj4m} spirit, ${username} managed to ${v1m} the danger and continue the quest. <p>`
 
 
 
 
 document.getElementById('beginning').innerHTML = beg;
+document.getElementById('middle').innerHTML = mid;
+document.getElementById('end').innerHTML = end; 
 document.getElementById('story').innerHTML = myStory;
