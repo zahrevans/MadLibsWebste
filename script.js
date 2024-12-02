dace96ac776054c9605839494ea16313d7f7a56b
zahrevans <526Zevans@frhsd.com>
2024-12-02 16:09:47 +0000

    Fixed grammer

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let v3b = prompt('Give a Verb');
 
 
 //Beginning paragraph
-let beg = `<p> Once upon a time, in a ${adj1b} village, there lived a ${n1b} named ${username}. Every day, ${username} would ${v1b} around the village, dreaming of ${n2b}. One morning, ${username} received an unexpected letter that said, “You have been chosen to find the legendary ${n2b}!” Excited, ${username} packed their ${adj2b} backpack and set off on a journey to find the ${n3b}, determined to ${n3b} it.</p>`;
+let beg = `<p> Once upon a time, in a ${adj1b} village, there lived a ${n1b} named ${username}. Every day, ${username} would ${v1b} around the village, dreaming of ${n2b}. One morning, ${username} received an unexpected letter that said, “You have been chosen to find the legendary ${n2b}!” Excited, ${username} packed their ${adj2b} backpack and set off on a journey to find the ${n2b}, determined to find it.</p>`;
 
 //middle variables
 let adj1m = prompt('Give an Adjective');
@@ -29,7 +29,7 @@ let adj4m = prompt('Give an Adjective');
 let mid = `<p> During the journey, ${username} faced many challenges. First, ${pron} had to cross a ${adj1m} river full of ${pn1m}. Then, ${username} found a ${adj2m} forest where a ${adj3m} ${animal} warned, “You must ${v1m} the ${n1m} or risk being trapped forever!” With quick thinking and a ${adj4m} spirit, ${username} managed to ${v1m} the danger and continue the quest. <p>`
 
 //end variables
-let n1e = prompt('Give a Noun')
+let n1e = prompt('Give a Noun (specifically a place)')
 let v1e = prompt('Give a Verb')
 let adj1e = prompt('Give an Adjective')
 let adj2e = prompt('Give an Adjective')
